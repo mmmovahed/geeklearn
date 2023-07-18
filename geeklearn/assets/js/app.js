6950a78f00a9d2791278ceaaac66fb5c6d4ad18d
github_username <you@domain.com>
2023-07-18 13:37:58 +0330

    set footer index file

--- geeklearn/assets/js/app.js
+++ geeklearn/assets/js/app.js
@@ -1,4 +1,5 @@
 import { Course } from "../../components/Course/course.js";
+import { Footer } from "../../components/Footer/footer.js";
 import { Header } from "../../components/Header/header.js";
 import { Menu } from "../../components/Menu/menu.js";
 import { Searchbox } from "../../components/Searchbox/searchbox.js";
@@ -8,3 +9,4 @@ window.customElements.define('site-header', Header)
 window.customElements.define('site-menu', Menu)
 window.customElements.define('site-search-box', Searchbox)
 window.customElements.define('site-course', Course)
+window.customElements.define('site-footer', Footer)
