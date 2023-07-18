cca0ae5d3603f5ee4d61ffe24546c5fd5a9c81fb
github_username <you@domain.com>
2023-07-18 13:34:14 +0330

    created footer component

---
+++ geeklearn/components/Footer/footer.js
@@ -0,0 +1,19 @@
+let template = document.createElement("template");
+template.innerHTML = `
+<link rel="stylesheet" href="./assets/font/font-awesome/all.min.css" />
+<link rel="stylesheet" href="./components/Footer/footer.css">
+<div>
+<h5>.کلیه حقوق برای گیک لرن محفوظ است</h5>
+</div>
+`;
+
+class Footer extends HTMLElement {
+  constructor() {
+    super();
+
+    this.attachShadow({ mode: "open" });
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
+  }
+}
+
+export { Footer };
