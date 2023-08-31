f0c605c48eb2b334b6bf29c09a9df1252de3fb95
Mohammad mahdi <work.mohamad.mahdi@gmail.com>
2023-08-30 20:17:55 -0700

    testing api json

--- tests/json.php
+++ tests/json.php
@@ -3,4 +3,6 @@ header("Content-Type: application/json; charset=UTF-8");
 
 $array=array('germany' => array('benz'=>['benz' => 'high' , 'id' => 4]), ['bmw' => 'high', 'id' => 78], ['audi' => 'low'], 'id'=>5,'name' => 'germany', 'korea' => ['kia', 'hunday'], 'iran' => ['saipa', 'iran khodro']);
 
-var_dump(json_encode($array));
+//echo(json_encode($array));
+$a=array('a'=>['x','y','z'],'b','c');
+echo json_encode($a);
