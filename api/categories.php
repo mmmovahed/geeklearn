57ba62c668ca656425bbf38d0286768e8faec3fe
Mohammad mahdi <work.mohamad.mahdi@gmail.com>
2023-09-02 09:43:19 -0700

    categories api has been updated

--- api/categories.php
+++ api/categories.php
@@ -1,14 +1,24 @@
 <?php
-
 header("Content-Type: application/json; charset=UTF-8");
 
-$conn = new mysqli("localhost", "root", "", "geeklearn_DB");
-$stmt = $conn->prepare("SELECT id, parents_id, name FROM tbl_courses_categories");
-$stmt->execute();
-$result = $stmt->get_result();
-$outp = $result->fetch_all(MYSQLI_ASSOC);
+$mysqli = new mysqli("localhost", "root", "", "geeklearn_DB");
 
-//var ($outp);
-echo json_encode($outp);
-$conn->close();
+$query = 'select id, parents_id, name from tbl_courses_categories order by parents_id';
+$result = $mysqli->query($query);
+$data = $result->fetch_all(MYSQLI_ASSOC);
+
+$menu = [];
+foreach ($data as $row) {
+    if ($row['parents_id'] == 0) {
+        $menu[$row['id']] = $row;
+        $menu[$row['id']]['submenus'] = [];
+    } else {
+        $menu[$row['parents_id']]['submenus'][] = $row;
+    }
+}
+
+echo json_encode(array($menu));
+
+$mysqli->close();
+
 ?>
