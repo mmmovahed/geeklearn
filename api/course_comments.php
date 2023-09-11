4839a36c58d1087c8ff5473c7c4ad4f97579ab85
Mohammad Mahdi <work.mohamad.mahdi@gmail.com>
2023-09-10 17:11:04 -0700

    api for commenting system has been developed(updated)

--- api/course_comments.php
+++ api/course_comments.php
@@ -1,4 +1,5 @@
 <?php
+$x= (($_GET["course"] != NULL) AND (isset($_GET["course"]))) ? $id=$_GET["course"]: header("Location: ../");
 
 header("Content-Type: application/json; charset=UTF-8");
 
@@ -7,7 +8,7 @@ $mysqli = new mysqli("localhost", "root", "", "geeklearn_DB");
 $query = '
 SELECT c.id,parents_id,title,comment,likes,dislikes,user_id,course_id,date,u.name, u.family
 FROM `tbl_courses_comments` c INNER JOIN tbl_users u
-WHERE c.status=1 AND c.course_id = 3 AND u.status>=1
+WHERE c.status=1 AND c.course_id = '.$id.' AND u.status>=1
 ';
 $result = $mysqli->query($query);
 $data = $result->fetch_all(MYSQLI_ASSOC);
