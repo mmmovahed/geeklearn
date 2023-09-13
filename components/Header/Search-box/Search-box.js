0229a934b116820df9e56e9241d2a612d10b9cec
github_username <you@domain.com>
2023-09-13 20:01:22 +0330

    changed course-cart component

--- components/Header/Search-box/Search-box.js
+++ components/Header/Search-box/Search-box.js
@@ -8,43 +8,37 @@ template.innerHTML = ` <link rel="stylesheet" href="./assets/font/font-awesome/a
     </div>
     <div class="search-container">
       <form class="main-search">
-      <input type="search" name="" id="search" placeholder="جستجو..." autocomplete="off">
+        <input type="search" name="" id="search" placeholder="جستجو..." autocomplete="off">
         <button type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
         <div class="serachResultContainer">
-         <div class="searchResult">
-          <div class="searchResultItemContainer">
-          <h4>آموزش پایتون</h4>
+          <div class="searchResult">
+            <div class="searchResultItemContainer">
+              <h4>آموزش پایتون</h4>
+            </div>
+            <div class="searchResultItemContainer">
+              <h4>آموزش پایتون</h4>
+            </div>
           </div>
-          <div class="searchResultItemContainer">
-          <h4>آموزش پایتون</h4>
-          </div>
-         </div>
         </div>
       </form>
     </div>
     <div class="information">
-
-    <div class="details">
-    <img src="./assets/image/video-library.png">
-    <span class="count">1000</span>
-    <span>دقیقه آموزش</span>
-   </div>
-     
-
+      <div class="details">
+        <img src="./assets/image/video-library.png">
+        <span class="count">1000</span>
+        <span>دقیقه آموزش</span>
+      </div>
       <div class="details">
         <img src="./assets/image/teacher-3.png">
         <span class="count">200</span>
         <span>مدرس مجرب</span>
       </div>
-
       <div class="details">
         <img src="./assets/image/student.png">
         <span class="count">500</span>
         <span>دانشجو</span>
       </div>
-
     </div>
-
   </div>
 </div>`;
 
@@ -54,15 +48,13 @@ class SearchBox extends HTMLElement {
     this.attachShadow({ mode: "open" });
     this.shadowRoot.appendChild(template.content.cloneNode(true));
     this.shadowRoot.querySelector("#search").addEventListener("focus", (e) => {
-      e.target.parentElement.classList.add('active')
+      e.target.parentElement.classList.add("active");
     });
     this.shadowRoot.querySelector("#search").addEventListener("blur", (e) => {
-      e.target.parentElement.classList.remove('active')
+      e.target.parentElement.classList.remove("active");
     });
-    if(window.location.href !== "http://geeklearn.ir/" )
-    {
-      this.shadowRoot.querySelector(".search-box").style.display = "none"
-      
+    if (window.location.href !== "http://geeklearn.ir/") {
+      this.shadowRoot.querySelector(".search-box").style.display = "none";
     }
   }
 }
--- index.php
+++ index.php
@@ -9,6 +9,8 @@ require_once "init.php";
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <link rel="stylesheet" href="./assets/css/style.css" />
   <link rel="stylesheet" href="./assets/font/font-awesome/all.min.css" />
+  <link rel="stylesheet" href="./assets/css/cdn.jsdelivr.net_npm_swiper@10_swiper-bundle.min.css">
+
   <title>GeekLearn</title>
 </head>
 
@@ -16,24 +18,29 @@ require_once "init.php";
 
   <div class="container">
     <site-header></site-header>
+    <photo-slider></photo-slider>
     <div class="courses-container">
-        <?php
-        $result = $main->showTheLatestCourses();
-        if ($result->num_rows > 0)
-            while($row = $result->fetch_assoc()) {
-            $cost=$row["cost"];
-                echo "<course-cart courseId='./course-page.php?id=$row[id]' course-title='".$row["title"]."' image=$row[thumbnail] price=".$cost." teacher='".$row["teacher_name"]."' duration='".$row["time"]."' teacherId='./teacher-page.php?id=".$row["teacher_id"]."' ></course-cart>";
-            }
+     <?php
+      $result = $main->showTheLatestCourses();
+      if ($result->num_rows > 0)
+        while ($row = $result->fetch_assoc()) {
+          $cost = $row["cost"];
+          echo "<course-cart courseId='./course-page.php?id=$row[id]' course-title='" . $row["title"] . "' image=$row[thumbnail] price=" . $cost . " teacher='" . $row["teacher_name"] . "' duration='" . $row["time"] . "' teacherId='./teacher-page.php?id=" . $row["teacher_id"] . "' ></course-cart>";
+        }
 
-        ?>
+      ?> 
 
     </div>
+    <course-slider>
+    </course-slider>
 
     <footer id="footer">
       <site-footer></site-footer>
     </footer>
   </div>
   <script type="module" src="./assets/js/app.js" defer></script>
+  <script src="https://cdn.jsdelivr.net/npm/swiper@10/swiper-element-bundle.min.js"></script>
+
 </body>
 
 </html>
